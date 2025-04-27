62e0639da42e16aed3ee9566c182d947f2183f59
Jayon Niravel <jayon.niravel@aeropay.com>
2025-04-27 11:06:32 -0500

    chore: setup of paper plane for react native demo app

---
+++ demo/syncReactNativeCLI/src/App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Demo React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, { useState } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+export default function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>
+        bow how
+      </Text>
+    </View>
+  );
+}
